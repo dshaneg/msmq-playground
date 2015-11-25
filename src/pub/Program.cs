acca69d86b47bf71f82017252bc53c356eda9c65
Shane Gibbons <shane.gibbons@gmail.com>
2015-11-24 22:48:53 -0600

    split pub and sub into separate console apps. Confirmed round robin behavior with multiple subscribers

--- src/pub/Program.cs
+++ src/pub/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
+using System.IO;
+using System.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace pub
@@ -10,6 +12,68 @@ namespace pub
     {
         static void Main(string[] args)
         {
+            var path = _AcquireQueuePath();
+
+            _EnsureQueue(path);
+
+            var cancelSource = new CancellationTokenSource();
+            var token = cancelSource.Token;
+
+            var task = _PopulateQueueAsync(path, token);
+            
+            Console.WriteLine("done. <Enter> to cancel the queue writer.");
+            Console.ReadLine();
+
+            cancelSource.Cancel();
+
+            Console.WriteLine("after cancel. about to exit.");
+            Console.ReadLine();
+        }
+
+        private static string _AcquireQueuePath()
+        {
+            var path = ConfigurationManager.AppSettings["queuePath"];
+
+            if (path == null)
+                throw new ConfigurationErrorsException("'queuePath' not defined in appsettings.");
+
+            return path;
+        }
+
+        private static void _EnsureQueue(string path)
+        {
+            if (!MessageQueue.Exists(path))
+            {
+                MessageQueue.Create(path);
+            }
+        }
+
+        private static async Task _PopulateQueueAsync(string path, CancellationToken token)
+        {
+            using (var q = new MessageQueue(path, QueueAccessMode.Send))
+            {
+                if (!q.CanWrite)
+                    throw new ApplicationException("Can't write to queue!");
+
+                for (var i = 0; i < 1000; i++)
+                {
+                    var msgText = "hello world " + i;
+
+                    var msg = new Message
+                    {
+                        Recoverable = true,
+                        BodyStream = new MemoryStream(Encoding.Default.GetBytes(msgText))
+                    };
+
+                    q.Send(msg);
+                    Console.WriteLine("{0}: Sent '{1}'.", DateTime.Now, msgText);
+
+                    await Task.Delay(TimeSpan.FromSeconds(1), token);
+
+                    if (token.IsCancellationRequested)
+                        break;
+                }
+            }
         }
     }
 }
--- src/sub/Program.cs
+++ src/sub/Program.cs
@@ -1,7 +1,9 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
+using System.Configuration;
+using System.IO;
+using System.Messaging;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace sub
@@ -10,6 +12,87 @@ namespace sub
     {
         static void Main(string[] args)
         {
+            var path = _AcquireQueuePath();
+
+            _EnsureQueue(path);
+
+            var cancelSource = new CancellationTokenSource();
+            var token = cancelSource.Token;
+
+            var task = _DrainQueue(path, token);
+
+            Console.WriteLine("done. <Enter> to cancel the queue reader.");
+            Console.ReadLine();
+
+            cancelSource.Cancel();
+
+            Console.WriteLine("after cancel. about to exit.");
+            Console.ReadLine();
+        }
+
+        private static string _AcquireQueuePath()
+        {
+            var path = ConfigurationManager.AppSettings["queuePath"];
+
+            if (path == null)
+                throw new ConfigurationErrorsException("'queuePath' not defined in appsettings.");
+
+            return path;
+        }
+
+        private static void _EnsureQueue(string path)
+        {
+            if (!MessageQueue.Exists(path))
+            {
+                MessageQueue.Create(path);
+            }
+        }
+
+        // returning async void!
+        private static async Task _DrainQueue(string path, CancellationToken token)
+        {
+            using (var q = new MessageQueue(path, QueueAccessMode.Receive))
+            {
+                if (!q.CanRead)
+                    throw new ApplicationException("Can't read from queue!");
+
+                while (!token.IsCancellationRequested)
+                {
+                    var message = await ReceiveAsync(q, token);
+                    if (message == null)
+                        continue;
+
+                    using (var bodyReader = new StreamReader(message.BodyStream))
+                    {
+                        var body = bodyReader.ReadToEnd();
+                        Console.WriteLine(body);
+                    }
+
+                    await Task.Delay(TimeSpan.FromMilliseconds(1), token);
+                }
+
+                Console.WriteLine("Canceled.");
+            }
+        }
+
+        public static Task<Message> ReceiveAsync(MessageQueue queue, CancellationToken cancelToken)
+        {
+            return Task<Message>.Factory.StartNew(() => Receive(queue, cancelToken), cancelToken);
+        }
+
+        private static Message Receive(MessageQueue queue, CancellationToken cancelToken)
+        {
+            var receiveAsyncResult = queue.BeginReceive();
+            WaitHandle.WaitAny(new[] { receiveAsyncResult.AsyncWaitHandle, cancelToken.WaitHandle });
+
+            Message message = null;
+
+            if (!cancelToken.IsCancellationRequested)
+            {
+                message = queue.EndReceive(receiveAsyncResult);
+            }
+
+            return message;
         }
     }
 }
